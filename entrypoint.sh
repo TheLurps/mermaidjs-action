538ab4f6f53ddc66cd9cf737b56161cd47920a6a
Joshua Schraven <joshua@schraven.email>
2022-09-19 21:49:12 +0200

    Debug search 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 FILE_PATTERN="$1"
 
-for INPUT_FILE in $(find /github/workspace -name $FILE_PATTERN); do
+echo $GITHUB_WORKSPACE
+echo $FILE_PATTERN
+for INPUT_FILE in $(find $GITHUB_WORKSPACE -name $FILE_PATTERN); do
     echo $INPUT_FILE
     #/home/mermaidcli/node_modules/.bin/mmdc \
     #    --puppeteerConfigFile /puppeteer-config.json \
