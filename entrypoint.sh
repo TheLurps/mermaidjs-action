b8c00ff27c022d29bd415581784a9917cdaa9087
Joshua Schraven <joshua@schraven.email>
2022-09-19 22:11:20 +0200

    Again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-FILES="$1"
+INPUT="$1"
 
-for FILE in $FILES; do
-    ls -lah $GITHUB_WORKSPACE/$FILE
-    #/home/mermaidcli/node_modules/.bin/mmdc \
-    #    --puppeteerConfigFile /puppeteer-config.json \
-    #    --input $INPUT_FILE
-done
+ls -lah $GITHUB_WORKSPACE/$FILE
+/home/mermaidcli/node_modules/.bin/mmdc \
+    --puppeteerConfigFile /puppeteer-config.json \
+    --input $GITHUB_WORKSPACE/$INPUT
