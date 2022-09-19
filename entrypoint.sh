6cc4dfd9db1289be63cf09976325e0cd7668d07c
Joshua Schraven <joshua@schraven.email>
2022-09-19 22:25:16 +0200

    Add more inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 INPUT="$1"
+OUTPUT="$2"
+BACKGROUNDCOLOR="$3"
+THEME="$4"
 
-echo "$GITHUB_WORKSPACE/$FILE"
-cat "$GITHUB_WORKSPACE/$FILE"
+mkdir -p "$GITHUB_WORKSPACE/$INPUT"
 
 /home/mermaidcli/node_modules/.bin/mmdc \
     --puppeteerConfigFile /puppeteer-config.json \
-    --input "$GITHUB_WORKSPACE/$INPUT"
+    --input "$GITHUB_WORKSPACE/$INPUT" \
+    --output "$GITHUB_WORKSPACE/$OUTPUT" \
+    --backgroundColor "$BACKGROUNDCOLOR" \
+    --theme "$THEME"
